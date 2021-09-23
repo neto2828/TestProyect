f7e502440a90e3583c0470b7a7ae39069f99fd4f
neto2828 <nemv.1311@gmail.com>
2021-09-23 05:13:38 -0500

    Se añadio la vista de Administrativo Se añadieron imagenes Se añadio la libreria de iconos font-awesome

--- TestProyect/Controllers/HomeController.cs
+++ TestProyect/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace TestProyect.Controllers
             return View();
         }
 
+        public IActionResult Administracion()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
