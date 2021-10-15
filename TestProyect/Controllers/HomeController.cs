f713c328e744e089a0d1f6f22ea831f3d4b1b9ef
neto2828 <nemv.1311@gmail.com>
2021-10-15 02:20:12 -0500

    Prueba con Azure

--- TestProyect/Controllers/HomeController.cs
+++ TestProyect/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using TestProyect.Models;
 
 namespace TestProyect.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- TestProyect/wwwroot/js/site.js
+++ TestProyect/wwwroot/js/site.js
@@ -4,3 +4,9 @@ window.onload = function () {
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
 }
+
+
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
